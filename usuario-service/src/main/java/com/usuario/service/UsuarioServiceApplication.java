78ac49389c3ffd53d9c7a87bc1dd5aa518271b87
yakgon <yratzgo@gmail.com>
2022-05-18 19:04:10 -0500

    Proyectos de microservicios

---
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -0,0 +1,17 @@
+package com.usuario.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+// Habilitar el cliente Feign para llamar a los demas microservicios parte del servicio usuario
+
+@EnableFeignClients
+@SpringBootApplication
+public class UsuarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServiceApplication.class, args);
+	}
+
+}
---
+++ usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
@@ -0,0 +1,27 @@
+package com.usuario.service.feignclients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.usuario.service.modelos.Moto;
+
+//Feign indica que es un cliente del servicio usuario que tiene como nombre "moto-service" y que se 
+//encuentra expuesto en la url-puerto ""http://localhost:8003"
+
+
+@FeignClient(name = "moto-service",url = "http://localhost:8003")
+@RequestMapping("/moto")
+public interface MotoFeignClient {
+
+	@PostMapping()
+	public Moto save(@RequestBody Moto moto);
+	
+	@GetMapping("/usuario/{usuarioId}")
+	public List<Moto> getMotos(@PathVariable("usuarioId") int usuarioId);
+}
